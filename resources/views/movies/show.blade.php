2d71621a5bf1b92e831899f17d2205040b4417be
G-Lumia <giorgiolumia@gmail.com>
2023-05-23 14:56:03 +0200

    Added movie details and added style in scss

--- resources/views/movies/show.blade.php
+++ resources/views/movies/show.blade.php
@@ -1,12 +1,32 @@
 @extends('layouts.app')
 
 @section('content')
-    <section class="container">
+    <section class="container d-flex">
         <div class="p-3">
             <h1>{{ $movie->title }}</h1>
             <p>{{ $movie->original_title }}</p>
             <img class="showImg" src="{{ $movie->image }}" alt="{{ $movie->title }}">
         </div>
-        <a href="{{ route('movies.index')}}"> Go back </a>
+        <div class="d-flex flex-column justify-content-center px-5">
+            <div>
+                <h3>Nationality: </h3>
+                <p>
+                    {{$movie->nationality}}
+                </p>
+            </div>
+            <div>
+                <h3>Date: </h3>
+                <p>
+                    {{$movie->date}}
+                </p>
+            </div>
+            <div>
+                <h3>Vote: </h3>
+                <p>
+                    {{$movie->vote}}
+                </p>
+            </div>
+        </div>
     </section>
+    <a href="{{ route('movies.index')}}"> Go back </a>
 @endsection
