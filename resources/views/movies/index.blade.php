22f5cf064f87b9fda8157ecc415b1ebd97c287c6
G-Lumia <giorgiolumia@gmail.com>
2023-05-23 14:44:18 +0200

    cards created

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -3,10 +3,14 @@
 @section('content')
     <section class="container">
         <h1>Your Movies</h1>
-        <ul>
+        <div class="row">
             @foreach ($movies as $movie)
-                <li><a href="{{ route('movies.show', ['id' => $movie->id]) }}">{{ $movie->title }}</a></li>
+            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 p-3 movieCard">
+                <a href="{{ route('movies.show', ['id' => $movie->id]) }}">{{ $movie->title }}
+                    <img src="{{ $movie->image}}" alt="{{ $movie['title'] }}">
+                </a>
+            </div>
             @endforeach
-        </ul>
+        </div>
     </section>
 @endsection
--- resources/views/movies/show.blade.php
+++ resources/views/movies/show.blade.php
@@ -2,9 +2,11 @@
 
 @section('content')
     <section class="container">
-        <h1>{{ $movie->title }}</h1>
-        <p>{{ $movie->original_title }}</p>
-        <img src="{{ $movie->image }}" alt="{{ $movie->title }}">
-
+        <div class="p-3">
+            <h1>{{ $movie->title }}</h1>
+            <p>{{ $movie->original_title }}</p>
+            <img class="showImg" src="{{ $movie->image }}" alt="{{ $movie->title }}">
+        </div>
+        <a href="{{ route('movies.index')}}"> Go back </a>
     </section>
 @endsection
